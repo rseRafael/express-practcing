f54b488db36a4d65d848425cb8a5cc9f9fbc45fa
@rseRafael <rafaelsantos.rse@gmail.com>
2017-07-01 17:08:26 -0300

    --

--- express-practicing/index.js
+++ express-practicing/index.js
@@ -2,7 +2,8 @@ let express = require("express");
 let fs = require("fs");
 let tools = require("/home/rafael/Code-Projects/Nodejs-Tools/nodejs-tools.js");
 let app = express();
-app.use("/public",express.static('/home/rafael/Public'));
+let publicDir = '/home/rafael/Public'
+app.use("/public",express.static(publicDir));
 app.get("/", (req, res)=>{
   res.send("<h1>Welcome to this empty page</h1>");
 })
@@ -11,25 +12,50 @@ app.get("/", (req, res)=>{
         res.json(obj);
       })
     .get("/public", (req, res)=>{
-      fs.readdir("/home/rafael/Public", (err, files)=>{
+      fs.readdir(publicDir, (err, files)=>{
+        let arr = [];
         if(err){
-          console.log("An error has occured");
+          console.log("An error has occured while tring to open " + publicDir);
           console.log(err);
         }
         else{
-          console.log(tools.dissectObj(files)) ;
-          let str = `<div style="display:flex; flex-Direction: column; justify-Content: center; align-Items: center; background-color:rgba(10, 150, 255, 1); width: '50%'; border: 1px solid red;"\n`;
+          arr.push(`<ul>\n`);
           files.forEach(
             (file)=>{
-              if(typeof file === "string"){
-                str += `\t<a href="/public/${file}">${file}</a>\n`
-              }
+                if(file.indexOf(".") !== -1){
+                  console.log("achei um arquivo: " + file);
+                  arr.push(`<li><a href=${publicDir + "/" +  file}>${file}</a></li>\n`);
+                }
+                else{
+                  console.log("nao e um arquivo: " + file);
+                  let currentDir = publicDir+"/"+file
+                  fs.readdir(currentDir, (err, files)=>{
+                    if(err){
+                      console.log(currentDir + "is not a directory");
+                      arr.push(`<li>${file}</li>\n`);
+                    }
+                    else{
+                      console.log("nao teve erro em: " + currentDir);
+                      files.forEach(
+                        (file)=>{
+                            if(file.indexOf(".") !== -1){
+                              console.log("achei um arquivo: " + file);
+                              arr.push(`<li><a href=${publicDir + file}>${file}</a></li>\n`);
+                            }
+                            else{
+                              console.log("nao e um arquivo: " + file);
+                              arr.push(`<li>${file}</li>\n`);
+                            }
+                          }
+                        );
+                    }
+                  });
+                }
+              });
             }
-          );
-          str += "</div>\n";
-          console.log(str);
-          res.send(str);
-        }
+          let html = arr.join("");
+          console.log(html);
+          res.send(html);
       });
     });
 app.listen(8080, ()=>{console.log("http://127.0.0.1:8080/ is running.")});
